14f6a5249d55fee5920bf5e7bda2823f045517d5
hamgamsa <hamgamsa@gmail.com>
2019-07-18 22:22:29 +0900

    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
-import Navbar from "./components/navbar";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <body id="App" data-spy="scroll" data-target="#main-nav">
-    <Navbar />
-
-  </body>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
