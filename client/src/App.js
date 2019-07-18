dc506016a34c9ad2dffd64aa8857681778ecb470
hamgamsa <hamgamsa@gmail.com>
2019-07-18 21:45:00 +0900

    1

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Navbar from "./components/navbar";
+
+function App() {
+  return <body id="App" data-spy="scroll" data-target="#main-nav">
+    <Navbar />
+
+  </body>;
+}
+
+export default App;
