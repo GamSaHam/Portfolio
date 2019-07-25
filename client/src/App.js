2c2f4eeeacc3dcb1f91afdaac4cf6da33c76d92f
hamgamsa <hamgamsa@gmail.com>
2019-07-25 16:04:12 +0900

    1

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
-
-import Navbar from "./components/navbar";
-import ShowCase from "./components/showcase";
-
-import "./scss/App.scss";
-
+import logo from './logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import './App.css';
+//import './main.css'
 
 function App() {
-  return <div className="App" data-spy="scroll" data-target="#main-nav">
-    <Navbar />
-    <ShowCase />
-  </div>;
+  return (
+    <div className="App">
+
+      <FontAwesomeIcon icon={faArrowRight} /> Read More
+      <Accordion defaultActiveKey="0">
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+              Click me!
+      </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+              Click me!
+      </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+      <p class="bg-primary text-primary">.text-primary</p>
+    </div>
+  );
 }
 
 export default App;
